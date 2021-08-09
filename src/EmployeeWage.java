33e16a53ef9f4cd5753e9693885a433929e61cea
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 19:26:32 +0530

    [Nikhil] - Added uc2

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,11 +3,13 @@ import java.util.Random;
 public class EmployeeWage {
     public static int present=1;
     public static void main(String[] args) {
+        int hr=0;
         Random ran=new Random();
         int is_present=ran.nextInt(2);
         if (present==is_present)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+            hr=8;
+
+        int wage_earned=hr*20;
+        System.out.println("Wage earned by employee is Rs. " + wage_earned );
     }
 }
