4111b047d8a33a5e039c860d15c1d60d5ba7f2a9
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 19:45:35 +0530

    [Nikhil] - Added uc5

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,23 +4,25 @@ public class EmployeeWage {
     public static int present=1;
     public static int half_time=2;
     public static void main(String[] args) {
-        int hr=0;
-        Random ran=new Random();
-        int is_present=ran.nextInt(3);
+        int hr = 0;
+        int wage_earned = 0;
+        Random ran = new Random();
+        for (int day=1;day<=20;day++){
+            int is_present = ran.nextInt(3);
+            switch (is_present) {
+                case 1:
+                    hr = 8;
+                    break;
+                case 2:
+                    hr = 4;
+                    break;
+                case 0:
+                    hr = 0;
+                    break;
+            }
 
-        switch (is_present){
-            case 1:
-                hr=8;
-                break;
-            case 2:
-                hr=4;
-                break;
-            case 0:
-                hr=0;
-                break;
+            wage_earned = hr * 20 + wage_earned;
         }
-
-        int wage_earned=hr*20;
         System.out.println("Wage earned by employee is Rs. " + wage_earned );
     }
 }
