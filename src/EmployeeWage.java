e416ec50dba5f9b804d071756003c32753214f54
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 19:20:00 +0530

    [Nikhil] - Added uc1

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
 public class EmployeeWage {
+    public static int present=1;
     public static void main(String[] args) {
-        
+        Random ran=new Random();
+        int is_present=ran.nextInt(2);
+        if (present==is_present)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
