5f9e57fcc8cb6f65f37f554f4cfda21fe60fef24
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 19:37:53 +0530

    [Nikhil] - Added uc4

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -7,10 +7,18 @@ public class EmployeeWage {
         int hr=0;
         Random ran=new Random();
         int is_present=ran.nextInt(3);
-        if (present==is_present)
-            hr=8;
-        else if (half_time==is_present)
-            hr=4;
+
+        switch (is_present){
+            case 1:
+                hr=8;
+                break;
+            case 2:
+                hr=4;
+                break;
+            case 0:
+                hr=0;
+                break;
+        }
 
         int wage_earned=hr*20;
         System.out.println("Wage earned by employee is Rs. " + wage_earned );
