f64ec44b31f9fbdda2d7d454c2f01d491178e2a0
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-13 22:26:06 +0530

    [Nihkil] - Refactored the code uc7

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,33 +1,8 @@
 import java.util.Random;
 
 public class EmployeeWage {
-    public static int present=1;
-    public static int half_time=2;
-    public static void main(String[] args) {
-        int hr = 0;
-        int total_hr=0;
-        int wage_earned = 0;
-        Random ran = new Random();
-        for (int day=1;day<=20;day++){
-            int is_present = ran.nextInt(3);
-            switch (is_present) {
-                case 1:
-                    hr = 8;
-                    break;
-                case 2:
-                    hr = 4;
-                    break;
-                case 0:
-                    hr = 0;
-                    break;
-            }
-            total_hr=hr + total_hr;
 
-            if (day==20 || hr==100) {
-                break;
-            }
-        }
-        wage_earned = total_hr * 20;
-        System.out.println("Wage earned by employee is Rs. " + wage_earned );
+    public static void main(String[] args) {
+        EmployeeWageLogic.employeeWageLogic();
     }
 }
---
+++ src/EmployeeWageLogic.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class EmployeeWageLogic {
+
+    public static int present=1;
+    public static int half_time=2;
+
+    public static void employeeWageLogic(){
+        int hr = 0;
+        int total_hr=0;
+        int wage_earned = 0;
+        Random ran = new Random();
+        for (int day=1;day<=20;day++){
+            int is_present = ran.nextInt(3);
+            switch (is_present) {
+                case 1:
+                    hr = 8;
+                    break;
+                case 2:
+                    hr = 4;
+                    break;
+                case 0:
+                    hr = 0;
+                    break;
+            }
+            total_hr=hr + total_hr;
+
+            if (day==20 || hr==100) {
+                break;
+            }
+        }
+        wage_earned = total_hr * 20;
+        System.out.println("Wage earned by employee is Rs. " + wage_earned );
+    }
+}
