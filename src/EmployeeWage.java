32016d9bf031567fafbf08eaf5ae4940098d799e
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-16 01:33:03 +0530

    added 2 comapnies

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,6 +3,8 @@ import java.util.Random;
 public class EmployeeWage {
 
     public static void main(String[] args) {
-        EmployeeWageLogic.employeeWageLogic();
+        EmployeeWageLogic emp1 = new EmployeeWageLogic(20); // for comapny1
+        EmployeeWageLogic emp = new EmployeeWageLogic(25, 1); // for company2
+
     }
 }
--- src/EmployeeWageLogic.java
+++ src/EmployeeWageLogic.java
@@ -4,13 +4,26 @@ public class EmployeeWageLogic {
 
     public static int present=1;
     public static int half_time=2;
+    public static int totalDay;
+    private static int companyDay;
+
+
+    public EmployeeWageLogic(int company1){
+        companyDay = company1;
+        employeeWageLogic();
+    }
+
+    public EmployeeWageLogic(int company2 , int a){
+        companyDay = company2;
+        employeeWageLogic();
+    }
 
     public static void employeeWageLogic(){
         int hr = 0;
         int total_hr=0;
         int wage_earned = 0;
         Random ran = new Random();
-        for (int day=1;day<=20;day++){
+        for (int day=1;day<=companyDay;day++){
             int is_present = ran.nextInt(3);
             switch (is_present) {
                 case 1:
@@ -30,6 +43,6 @@ public class EmployeeWageLogic {
             }
         }
         wage_earned = total_hr * 20;
-        System.out.println("Wage earned by employee is Rs. " + wage_earned );
+        System.out.println("Wage earned by employee is Rs. " + wage_earned);
     }
 }
